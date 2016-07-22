2de20c61600a93c2b3f934e77cdad165ce09b3f2
Louis Popi <theh2o64@gmail.com>
2016-07-22 18:26:25 +0200

    oneplus2: More cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,8 +17,8 @@
 # inherit CodeAurora MSM8994 Board Config
 -include device/qcom/msm8994/BoardConfig.mk
 
-# Inherit from qcom-common
-include device/qcom/common/BoardConfigCommon.mk
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := device/oneplus/oneplus2/include
 
 # Assertions
 TARGET_OTA_ASSERT_DEVICE := OnePlus2,oneplus2
@@ -26,14 +26,6 @@ TARGET_BOARD_INFO_FILE ?= device/oneplus/oneplus2/board-info.txt
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
-
-# Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-KERNEL_DIR := kernel/oneplus/msm8994
-KERNEL_DEFCONFIG := oneplus2_defconfig
 
 # Audio
 AUDIO_FEATURE_ENABLED_ACDB_LICENSE := true
@@ -64,31 +56,31 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 QCOM_BT_USE_BTNV := true
 QCOM_BT_USE_SMD_TTY := true
 
-# Camera
-USE_DEVICE_SPECIFIC_CAMERA := true
+# Dex-opt
+# Enable dex pre-opt to speed up initial boot
+ifeq ($(HOST_OS),linux)
+      WITH_DEXPREOPT := true
+endif
 
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := device/oneplus/oneplus2/include
+# Gestures
+TARGET_GESTURES_NODE := "/proc/touchpanel/gesture_enable"
+
+# GPS
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 
 # Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_LIBINIT_DEFINES_FILE := device/oneplus/oneplus2/init/init_oneplus2.cpp
 
-# Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HAS_QCOM_WLAN_SDK := true
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-TARGET_USES_WCNSS_CTRL := true
-TARGET_USES_QCOM_WCNSS_QMI := true
-WIFI_DRIVER_FW_PATH_AP := "ap"
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive # Selinux permissive for bringup
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+KERNEL_DIR := kernel/oneplus/msm8994
+KERNEL_DEFCONFIG := oneplus2_defconfig
+TARGET_RECOVERY_FSTAB := device/oneplus/oneplus2/ramdisk/fstab.qcom
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
@@ -96,20 +88,35 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 59047394304
-BOARD_FLASH_BLOCK_SIZE := 262144
-TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := false
 
-# Recovery
-TARGET_RECOVERY_FSTAB := device/oneplus/oneplus2/ramdisk/fstab.qcom
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
 
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := false
+
+# Sepolicy
+include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/oneplus/oneplus2/sepolicy
 
-# Gestures
-TARGET_GESTURES_NODE := "/proc/touchpanel/gesture_enable"
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus2/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -14,10 +14,9 @@
 # limitations under the License.
 #
 
+# Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/oneplus2/oneplus2-vendor.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus2/overlay
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
@@ -27,43 +26,56 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/oneplus/oneplus2/prebuilt/system,system)
 
+# Dalvik/HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=96 \
+    ro.hwui.layer_cache_size=64 \
+    ro.hwui.path_cache_size=39 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=7 \
+    ro.hwui.r_buffer_cache_size=12 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=3072 \
+    ro.hwui.text_large_cache_height=4096
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus2/overlay
+PRODUCT_PACKAGE_OVERLAYS += device/oneplus/oneplus2/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG += xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # CAF Branch
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.par.branch=LA.BF64.1.2.2-06140-8x94.0
 
-# Dalvik/HWUI
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
+# CodeAurora MSM8994 Device Tree
+$(call inherit-product, device/qcom/msm8994/msm8994.mk)
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# Haters gonna hate..
+# Haters gonna hate ..
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Camera
 PRODUCT_PACKAGES += \
     libop2_cam
 
+# USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
 # Doze mode
 PRODUCT_PACKAGES += \
     OneplusDoze
 
-# Fingerprint sensor
-PRODUCT_PACKAGES += \
-    fingerprintd
-
-# Keystore
-PRODUCT_PACKAGES += \
-    keystore.msm8994 \
-    keystore.qcom
-
-# Power
-PRODUCT_PACKAGES += \
-    power.msm8994 \
-    power.qcom
-
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
+# Call the proprietary setup
+$(call inherit-product-if-exists, vendor/oneplus/oneplus2/oneplus2-vendor.mk)
